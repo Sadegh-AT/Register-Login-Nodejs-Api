114ae922b68c8c034fdecf67510079a18c24387a
SadeghAT <sadegh.artivox@gmail.com>
2023-02-01 18:34:14 +0330

    send request from frontend to api and insert data to local db

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,15 @@ let db = {
 app.use(cors());
 app.use(bodyParser.json());
 
-app.post("/api/add-user", (req, res) => {});
+app.post("/api/add-user", (req, res) => {
+  let newUser = {
+    id: db.users.length + 1,
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  };
+  db.users.push(newUser);
+  console.log(db.users);
+});
 
 app.listen(3000);
