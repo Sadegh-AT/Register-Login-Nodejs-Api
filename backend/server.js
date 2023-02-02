af88837e283f34b9caa1c5f6571ab697aeaa057b
SadeghAT <sadegh.artivox@gmail.com>
2023-02-02 17:44:30 +0330

    add more input and add admin panel html file

--- backend/server.js
+++ backend/server.js
@@ -2,29 +2,3 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-
-let db = {
-  users: [
-    {
-      id: 1,
-      username: "sadegh",
-      email: "sadegh.artivox@gmail.com",
-      password: "1234",
-    },
-  ],
-};
-app.use(cors());
-app.use(bodyParser.json());
-
-app.post("/api/add-user", (req, res) => {
-  let newUser = {
-    id: db.users.length + 1,
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-  };
-  db.users.push(newUser);
-  console.log(db.users);
-});
-
-app.listen(3000);
--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,26 +1,6 @@
 const form = document.querySelector(".form");
+const name = document.querySelector("#name");
+const family = document.querySelector("#family");
 const username = document.querySelector("#username");
-const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 const submit = document.querySelector("#submit");
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  let newUser = {
-    username: username.value,
-    email: email.value,
-    password: password.value,
-  };
-
-  fetch("http://localhost:3000/api/add-user", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(newUser),
-  })
-    .then((res) => res.text())
-    .then((data) => {
-      console.log(data);
-    });
-});
