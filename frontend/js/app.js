2009b9b9ddb3ffdcce7a6deb645ff47fd2973c34
SadeghAT <sadegh.artivox@gmail.com>
2023-02-03 20:32:52 +0330

    add error style to null input

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -7,19 +7,57 @@ const submit = document.querySelector("#submit");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  if (validateInput(name, family, username, password) == false) {
+    let newUser = {
+      name: name.value,
+      family: family.value,
+      username: username.value,
+      password: password.value,
+    };
 
-  let newUser = {
-    name: name.value,
-    family: family.value,
-    username: username.value,
-    password: password.value,
-  };
-
-  fetch("http://localhost:3000/api/users/new-user", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(newUser),
-  }).then((res) => console.log(res));
+    fetch("http://localhost:3000/api/users/new-user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newUser),
+    }).then((res) => console.log(res));
+    clearInput();
+  }
 });
+
+function validateInput(name, family, username, password) {
+  name.value = name.value.trim();
+  family.value = family.value.trim();
+  username.value = username.value.trim();
+  password.value = password.value.trim();
+
+  let inputs = [name, family, username, password];
+
+  inputs.forEach((input) => {
+    if (input.value == "") {
+      SeterrorStyle(input);
+      RemoveErrorStyle(input);
+    } else {
+      console.log("check");
+      return false;
+    }
+  });
+}
+
+function clearInput() {
+  name.value = "";
+  family.value = "";
+  username.value = "";
+  password.value = "";
+}
+
+function SeterrorStyle(input) {
+  input.classList.add("error");
+}
+
+function RemoveErrorStyle(input) {
+  input.addEventListener("input", () => {
+    input.classList.remove("error");
+  });
+}
