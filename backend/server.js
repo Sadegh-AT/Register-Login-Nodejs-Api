c6d952ffaf3d82a3789a42b199f5f83614b3703a
SadeghAT <sadegh.artivox@gmail.com>
2023-02-04 22:52:58 +0330

    make Get Request Api and add user card to admin panel

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,12 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 const userRoute = require("./routes/usersRoute");
+const adminRoute = require("./routes/adminRoute");
 
 app.use(bodyParser.json());
 app.use(cors());
 
 app.use("/api/users", userRoute);
+app.use("/api/users", adminRoute);
 
 app.listen(3000);
