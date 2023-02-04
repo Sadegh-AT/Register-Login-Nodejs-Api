84e17c9b764c48577b480abbc3df9b00e3479474
SadeghAT <sadegh.artivox@gmail.com>
2023-02-04 23:38:50 +0330

    add eventListener to delete modal for hide

--- frontend/js/admin.js
+++ frontend/js/admin.js
@@ -1,7 +1,9 @@
 const usersDiv = document.querySelector(".usres");
 const DeleteBtn = document.querySelectorAll(".delete-btn");
 const EditBtn = document.querySelectorAll(".edit-btn");
-
+const BlackDiv = document.querySelector(".black-div");
+const DeleteModal = document.querySelector(".delete-modal");
+BlackDiv.addEventListener("click", HideDeleteModal);
 window.addEventListener("load", function () {
   fetch("http://localhost:3000/api/users")
     .then((res) => res.json())
@@ -23,11 +25,20 @@ function UserCard(username, date) {
                         <h6 class="info-date">${date}</h6>
                     </div>
                     <div class="buttons">
-                        <a class="delete-btn" onclick="Hello()"><i class="fa-solid fa-trash"></i></a>
+                        <a class="delete-btn" onclick="ShowDeleteModel()"><i class="fa-solid fa-trash"></i></a>
                         <a class="edit-btn" onclick="Hello()"><i class="fa-solid fa-user-pen"></i></a>
                     </div>
                 </div>
 
             </div>`;
   usersDiv.insertAdjacentHTML("afterbegin", card);
+}
+
+function ShowDeleteModel() {
+  BlackDiv.classList.add("active");
+  DeleteModal.classList.add("active");
+}
+function HideDeleteModal() {
+  BlackDiv.classList.remove("active");
+  DeleteModal.classList.remove("active");
 }
